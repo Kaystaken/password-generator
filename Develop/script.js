fc66acee9ba6f6ee1bb6c7e3a8a1150d57be85a9
Kay <hiebertkay@gmail.com>
2023-11-15 21:11:33 -0500

    added more comments in script.js, added alert if no characters were selected and generated password if criteria is met

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ function generatePassword() {
   var upperCaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var lowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
   var numericCharacters = '0123456789';
-  var specialCharacters = '!#$%&()*+,-./:;<=>?@[\]^_{|}~';
+  var specialCharacters = '!#$%&()*+,-./:;<=>?@[]^_{|}~';
   var valid = false;
   var length = "";
 
@@ -22,19 +22,38 @@ function generatePassword() {
     length = prompt("How long do you want your password?");
     //parseInt has its own return and is its own function
     length = parseInt(length, 10);
-    valid = !(length <= 8 || length >= 128 || Number.isNaN(length));
+    valid = !(length < 8 || length > 128 || Number.isNaN(length));
     if (!valid) {
       //length chosen to meet acceptance critera
       alert("Please choose a length of at least 8 characters and no more than 128 characters");
     }
   }
-
+  //confirm displays my questions to user asking if they'd like to include each character type
   var includeLowerCase = confirm("Would you like to include lowercase characters?");
-  var includeUppererCase = confirm("Would you like to include uppercase characters?");
+  var includeUpperCase = confirm("Would you like to include uppercase characters?");
   var includeNumeric = confirm("Would you like to include numeric characters?");
   var includeSpecial = confirm("Would you like to include special characters?");
 
-  var characters = upperCaseCharacters + lowerCaseCharacters + numericCharacters + specialCharacters;
+  // if no character is selected user is alerted that they must choose at least 1
+  if (!includeLowerCase && !includeUpperCase && !includeNumeric && !includeSpecial) {
+    alert("Password can't be generated please include at least 1 character type"); 
+    return ""; 
+  }
+
+  var characters = "";
+  if (includeLowerCase) {
+    characters += lowerCaseCharacters;
+  }
+  if (includeUpperCase) {
+    characters += upperCaseCharacters;
+  }
+  if (includeNumeric) {
+    characters += numericCharacters;
+  }
+  if (includeSpecial) {
+    characters += specialCharacters;
+  }
+
   var password = "";
   //++ increase by 1 "increment operator"
   for (var count = 0; count < length; count++) {
